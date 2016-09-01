50418f8fc4f03752404de4a2da285a76e7d8b25e
Tamamo <lordszynencja@gmail.com>
2016-09-01 09:12:50 +0200

    draw
    Poly.draw function added.

--- poly.js
+++ poly.js
@@ -2,9 +2,12 @@ class Poly {
 	draw(c) {
 		var l=this.v.length;
 		ctx.beginPath();
+		ctx.moveTo(this.v[l-1][0],this.v[l-1][1]);
 		for (var i=0;i<l;i++) {
 			ctx.fillStyle = (i==c ? hold_color : (colliding ? collide_color : color));
+			ctx.lineTo(this.v[i][0],this.v[i][1]);
 		}
+		ctx.stroke();
 	}
 	
 	add(v) {
