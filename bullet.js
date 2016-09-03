e9fadeecc09e24773d10b24d3c23ed935716adc1
Tamamo <lordszynencja@gmail.com>
2016-09-03 12:06:42 +0200

    collision system upgraded
    better collision detection for bullets and collision detection for
    polygons added, convex hulls can be added manually

--- bullet.js
+++ bullet.js
@@ -5,6 +5,8 @@ class Bullet {
 		var collided = false;
 		var leftx = this.vx*this.vleft;
 		var lefty = this.vy*this.vleft;
+		var best = Math.sqrt(this.vx*this.vx+this.vy*this.vy)+1;
+		var data = false;//[ang,cr[0],cr[1]]
 		for (i=0;i<l;i++) {
 			var x1 = v[i][0];
 			var y1 = v[i][1];
@@ -14,19 +16,25 @@ class Bullet {
 			var ver2 = [[this.x,this.y],[this.x+leftx,this.y+lefty]];
 			var cr = cross(ver1,ver2);
 			if (cr !== false) {
-				var distance = dist(cr,[this.x,this.y]);
-				var ang1 = angle(this.vx,this.vy);
-				var ang2 = angle(x2-x1,y2-y1);
-				var ang = 2*(ang2-ang1);
-				var newvx = (Math.cos(ang)*this.vx-Math.sin(ang)*this.vy)*boing_factor;
-				var newvy = (Math.sin(ang)*this.vx+Math.cos(ang)*this.vy)*boing_factor;
-				this.vx = newvx;
-				this.vy = newvy;
-				this.vleft -= distance;
-				this.x = cr[0]+0.001*this.vx;
-				this.y = cr[1]+0.001*this.vy;
-				collided = true;
+				var d = dist(cr,[this.x,this.y]);
+				if (d<best) {
+					best = d;
+					var ang1 = angle(this.vx,this.vy);
+					var ang2 = angle(x2-x1,y2-y1);
+					data = [2*(ang2-ang1),cr[0],cr[1]];
+				}
 			}
+		}
+		if (data !== false) {
+			var ang = data[0];
+			var newvx = (Math.cos(ang)*this.vx-Math.sin(ang)*this.vy)*boing_factor;
+			var newvy = (Math.sin(ang)*this.vx+Math.cos(ang)*this.vy)*boing_factor;
+			this.vx = newvx;
+			this.vy = newvy;
+			this.vleft -= best;
+			this.x = data[1]+0.001*this.vx;
+			this.y = data[2]+0.001*this.vy;
+			collided = true;
 		}
 		return collided;
 	}
@@ -35,16 +43,16 @@ class Bullet {
 		this.vleft = 1;
 		var col = polies.length;
 		var i;
-		//console.log(this.x+" "+this.y);
 		while (col>=0 && this.vleft>0.001) {
 			col = -1;
 			for (i=1;i<polies.length;i++) if (i!=col && this.collide(polies[i].v)) col = i;
 		}
-		//console.log("after:"+this.x+" "+this.y);
 		if (this.vleft>0.001) {
 			this.x+=this.vx*this.vleft;
 			this.y+=this.vy*this.vleft;
 		}
+		if (this.x<0 || this.x>canvas.width || this.y<0 || this.y>canvas.widths) return true;
+		return false;
 	}
 	
 	draw() {
@@ -59,6 +67,5 @@ class Bullet {
 		this.y=y;
 		this.vx=vx;
 		this.vy=vy;
-		console.log("bullet "+[x,y,vx,vy]);
 	}
 }
